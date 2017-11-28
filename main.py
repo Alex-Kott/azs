28f28e7c52a87372adb4e49326723c115d9fed44
Alexey Kott <alexey.kott@gmail.com>
2017-11-29 01:58:18 +0300

    Add code to Item_Name and Item_Priece

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from time import sleep
 import argparse
 from xml.etree.ElementTree import Element, SubElement, tostring, parse
@@ -15,7 +16,7 @@ def prettify(elem):
 	"""
 	rough_string = tostring(elem, 'utf-8')
 	reparsed = minidom.parseString(rough_string)
-	return reparsed.toprettyxml(indent="\t")
+	return reparsed.toprettyxml(indent="\t", encoding="utf8")
 
 
 def get_pretty_names(args):
@@ -33,13 +34,13 @@ def get_raw_data(args):
 	tree = parse(args.raw_file)
 	root = tree.getroot()
 	raw_data = []
-	for item in root.findall('Цена_x0020_Прайса_x0020_Розничная'):
+	for item in root.findall(u'Цена_x0020_Прайса_x0020_Розничная'):
 		good = {}
-		good['price_date'] = item.find('Цена_x0020_Прайса_x0020_Розничная_x0020_Дата').text
-		good['retail_price'] = item.find('Цена_x0020_Прайса_x0020_Розничная').text
-		good['name'] = item.find('Номенклатура_x0020_Наименование').text
-		good['gas_station'] = item.find('Объект_x0020_Управления_x0020_Родитель_x0020_ASPB').text
-		good['code'] = item.find('Номенклатура_x0020_Эталон_x0020_Код').text
+		good['price_date'] = item.find(u'Цена_x0020_Прайса_x0020_Розничная_x0020_Дата').text
+		good['retail_price'] = item.find(u'Цена_x0020_Прайса_x0020_Розничная').text
+		good['name'] = item.find(u'Номенклатура_x0020_Наименование').text
+		good['gas_station'] = item.find(u'Объект_x0020_Управления_x0020_Родитель_x0020_ASPB').text
+		good['code'] = item.find(u'Номенклатура_x0020_Эталон_x0020_Код').text
 		raw_data.append(good)
 	return raw_data
 
@@ -48,9 +49,9 @@ def generate_file(good_names, raw_data):
 	for i in raw_data:
 		item = SubElement(menu, 'Item')
 		code = int(i['code'])
-		item_name = SubElement(item, 'Item_Name')
+		item_name = SubElement(item, 'Item_Name_{}'.format(i['code']))
 		item_name.text = good_names.get(code, i['name'])
-		item_price = SubElement(item, 'Item_Price')
+		item_price = SubElement(item, 'Item_Price_{}'.format(i['code']))
 		item_price.text = i['retail_price']
 
 	with open(args.output_file, 'w') as f:
@@ -77,6 +78,11 @@ class Args:
 
 if __name__ == "__main__":
 	args = Args()
+
+	# with open(args.output_file, 'w') as f:
+	# 	f.write(u'cdsc')
+	# 	print(type(u'scsd'))
+	# exit()
 	
 	while True:
 		if check_changes():
